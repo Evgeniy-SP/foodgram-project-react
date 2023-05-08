be129c490decccb01bba97d951f10713b714b340
Евгений Кудрявцев <evgeniy-sp@yandex.ru>
2023-05-08 15:55:06 +0300

    back

---
+++ backend/users/models.py
@@ -0,0 +1,48 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    """
+    Кастомная модель пользователя.
+    """
+    username = models.CharField(
+        db_index=True,
+        max_length=150,
+        unique=True,
+        verbose_name='Уникальное имя',
+        help_text='Введите уникальное имя пользователя'
+    )
+    email = models.EmailField(
+        db_index=True,
+        unique=True,
+        max_length=254,
+        verbose_name='Электронная почта',
+        help_text='Введите электронную почту пользователя'
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя',
+        help_text='Введите имя пользователя'
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия',
+        help_text='Введите фамилию пользователя'
+    )
+    is_subscribed = models.BooleanField(
+        default=False,
+        verbose_name='Подписка на данного пользователя',
+        help_text='Отметьте для подписки на данного пользователя'
+    )
+    password = models.CharField(
+        max_length=150,
+        verbose_name='Пароль',
+        help_text='Введите пароль'
+    )
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'password']
+
+    def __str__(self):
+        """Строковое представление модели ."""
+        return self.username
