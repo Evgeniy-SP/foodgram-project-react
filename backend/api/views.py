ad471eae044d4f1a7f225f882ae1b58a06d78ed8
Евгений Кудрявцев <evgeniy-sp@yandex.ru>
2023-05-13 20:56:33 +0300

    fix canvas_method

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,9 @@
 from http import HTTPStatus
 
 from django.db.models import Sum
-from django.http import HttpResponse
 from django.shortcuts import get_list_or_404, get_object_or_404
 from djoser.views import UserViewSet
 from django_filters.rest_framework import DjangoFilterBackend
-from reportlab.lib.pagesizes import A4
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.pdfgen import canvas
 from rest_framework import permissions, viewsets
 from rest_framework.response import Response
 
@@ -22,6 +17,7 @@ from recipes.models import (
     Tag
 )
 from users.models import User
+from .utils import canvas_method
 from .filters import IngredientSearchFilter, RecipeFilters
 from .serializers import (
     CartSerializer,
@@ -177,43 +173,6 @@ class DownloadCart(viewsets.ModelViewSet):
     """
     permission_classes = [permissions.IsAuthenticated]
 
-    @staticmethod
-    def canvas_method(dictionary):
-        """
-        Сохранение списка покупок в формате PDF.
-        """
-        response = HttpResponse(content_type='application/pdf')
-        response[
-            'Content-Disposition'
-        ] = 'attachment; filename = "shopping_cart.pdf"'
-        begin_position_x, begin_position_y = 40, 650
-        sheet = canvas.Canvas(response, pagesize=A4)
-        pdfmetrics.registerFont(TTFont('FreeSans', 'data/FreeSans.ttf'))
-        sheet.setFont('FreeSans', 50)
-        sheet.setTitle('Список покупок')
-        sheet.drawString(
-            begin_position_x,
-            begin_position_y + 40,
-            'Список покупок: '
-        )
-        sheet.setFont('FreeSans', 24)
-        for number, item in enumerate(dictionary, start=1):
-            if begin_position_y < 100:
-                begin_position_y = 700
-                sheet.showPage()
-                sheet.setFont('FreeSans', 24)
-            sheet.drawString(
-                begin_position_x,
-                begin_position_y,
-                f'{number}.  {item["ingredient__name"]} - '
-                f'{item["ingredient_total"]}'
-                f' {item["ingredient__measurement_unit"]}'
-            )
-            begin_position_y -= 30
-        sheet.showPage()
-        sheet.save()
-        return response
-
     def download(self, request):
         """
         Создания списка покупок.
@@ -224,4 +183,4 @@ class DownloadCart(viewsets.ModelViewSet):
             'ingredient__name',
             'ingredient__measurement_unit'
         ).order_by('ingredient__name').annotate(ingredient_total=Sum('amount'))
-        return self.canvas_method(result)
+        return canvas_method(result)
